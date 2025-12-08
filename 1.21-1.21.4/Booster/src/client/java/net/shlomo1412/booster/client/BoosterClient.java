a77bac0efa94b56a87e57232f469149761ab9acd
Shlomo1412 <shlomofi050@gmail.com>
2025-12-08 19:42:53 +0200

    first commit

---
+++ 1.21-1.21.4/Booster/src/client/java/net/shlomo1412/booster/client/BoosterClient.java
@@ -0,0 +1,10 @@
+package net.shlomo1412.booster.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BoosterClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ 1.21-1.21.4/Booster/src/main/java/net/shlomo1412/booster/Booster.java
@@ -0,0 +1,10 @@
+package net.shlomo1412.booster;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Booster implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
